9e6297ca427625f2a7cb47f18d6437b62fe75bf2
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-09-30 01:36:54 +0300

    Add placeholder view for /

--- completion_pmarket/views.py
+++ completion_pmarket/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Market, Outcome
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    markets_list = Market.objects.order_by('-start_date')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'markets_list': markets_list,
+    }
+    return HttpResponse(template.render(context, request))
