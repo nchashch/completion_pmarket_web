372d91adbd9a2dda17eff81d5deac72d27bd0a27
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-09-30 00:04:58 +0300

    Register completion_pmarket models in admin site

--- completion_pmarket/admin.py
+++ completion_pmarket/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Portfolio
+from .models import Market
+from .models import Outcome
+from .models import Position
+from .models import Order
 
-# Register your models here.
+admin.site.register(Portfolio)
+admin.site.register(Market)
+admin.site.register(Outcome)
+admin.site.register(Position)
+admin.site.register(Order)
