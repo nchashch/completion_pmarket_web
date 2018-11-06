8af2ffdeb8f657635f31af1d0f804f46d89a6594
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-11-06 22:04:03 +0300

    Add outcome view
    * Use ?pk=* instead of ?m=* and ?o=* in index and market views respectively

--- completion_pmarket/urls.py
+++ completion_pmarket/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('market', views.market, name='market'),
+    path('outcome', views.outcome, name='outcome'),
 ]
 
--- completion_pmarket/views.py
+++ completion_pmarket/views.py
@@ -19,14 +19,14 @@ def portfolio(request):
 def market(request):
     outcomes_list = []
     market = None
-    template = loader.get_template('market.html')
     if request.GET:
-        market = request.GET['m']
+        market = request.GET['pk']
         outcomes_list = Outcome.objects.order_by('-outcome_date').filter(market=market)
         try:
             market = Market.objects.get(pk=market)
         except django.core.exceptions.ObjectDoesNotExist:
             market = []
+    template = loader.get_template('market.html')
     context = {
         'outcomes_list': outcomes_list,
         'market': market,
@@ -34,7 +34,18 @@ def market(request):
     return HttpResponse(template.render(context, request))
 
 def outcome(request):
-    pass
+    outcome = []
+    if request.GET:
+        outcome = request.GET['pk']
+        try:
+            outcome = Outcome.objects.get(pk=outcome)
+        except django.core.exceptions.ObjectDoesNotExist:
+            outcome = []
+    template = loader.get_template('outcome.html')
+    context = {
+        'outcome': outcome,
+    }
+    return HttpResponse(template.render(context, request))
 
 def position(request):
     pass
