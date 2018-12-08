88d35982a5eb09e11e45d8baaff33938a5f4faac
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-12-08 20:55:42 +0300

    Use DateField instead of DateTimeField in models

--- completion_pmarket/models.py
+++ completion_pmarket/models.py
@@ -11,13 +11,13 @@ class Market(models.Model):
     name = models.CharField(max_length=256)
     b = models.FloatField(default=0.0)
     number_of_outcomes = models.IntegerField(default=1)
-    start_date = models.DateTimeField('start date')
-    end_date = models.DateTimeField('end date')
+    start_date = models.DateField('start date')
+    end_date = models.DateField('end date')
 
 # Public
 class Outcome(models.Model):
     market = models.ForeignKey(Market, on_delete=models.CASCADE)
-    outcome_date = models.DateTimeField('outcome date')
+    outcome_date = models.DateField('outcome date')
     outstanding = models.IntegerField(default=0)
     probability = models.FloatField(default=0.0)
 
